f453761bba9c3d088c90cf1f8669c41ecbee9a38
Dmitry Velichko <soleil-2005@mail.ru>
2023-01-03 23:49:06 +0400

    Add Navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -2,6 +2,37 @@ import React, {useState, useEffect, useContext} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+// import internal
+import Style from './Navbar.module.css'
+import images from '../../assets'
+import {Model, TokenList} from '../index'
 
+const NavBar = () => {
+  const menuItems = [
+    {
+      name: 'Swap',
+      link: '/'
+    },
+    {
+      name: 'Tokens',
+      link: '/'
+    },
+    {
+      name: 'Pools',
+      link: '/'
+    }
+  ]
+
+  const [openModel, setOpenModel] = useState(false)
+  const [openTokenBox, setOpenTokenBox] = useState(false)
+
+  return (
+    <div className={Style.NavBar}>
+      <div className={Style.NavBar_box}>
+        <div className={Style.NavBar_box_left}>
+          {/* LOGO IMAGE */}
+    
+  )
+}
 
 export default NavBar
